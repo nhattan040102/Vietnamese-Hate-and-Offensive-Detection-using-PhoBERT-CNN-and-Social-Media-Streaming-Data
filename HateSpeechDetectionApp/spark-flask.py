e5f8c232f0b4264e5b5eede284c6fd0abf104b18
nhattan_work <tantran.040102@gmail.com>
2023-11-19 23:14:52 +0700

    refactor: update code structure in flask app

--- HateSpeechDetectionApp/spark-flask.py
+++ HateSpeechDetectionApp/spark-flask.py
@@ -13,7 +13,7 @@ import sys
 
 consumer = KafkaConsumer('cleanData', bootstrap_servers=['localhost:9092'])
 app = Flask(__name__)
-url = "https://www.youtube.com/watch?v=SOTamWNgDKc&t=27363s"
+url = "https://www.youtube.com/watch?v=lhznO_xsbfU"
 time_list = []
 
 @app.route('/home')
@@ -22,33 +22,24 @@ def home():
 
 @app.route('/table-data', methods=['GET','POST'])
 def table_data():
+    def get_stream_data():        
+        try:
+            while True:
+                for msg in consumer:
+                    print('received')
+                    record = json.loads(msg.value.decode('utf-8'))
+                    if 'timestamp' in record:
+                        record['timestamp'] = record['timestamp'][:19]
+                    print(record)
+                    
+                    yield (f"data:{json.dumps(record)}\n\n")
+
+                    time.sleep(10)
+        except KeyboardInterrupt:                                    
+            print('Stop streaming data')        
+    
     return Response(get_stream_data(),mimetype="text/event-stream")
 
-def get_stream_data():
-    try:
-        for msg in consumer:
-            print('received')
-            record = json.loads(msg.value.decode('utf-8'))
-            if 'timestamp' in record:
-                record['timestamp'] = record['timestamp'][:19]
-            print(record)
-            # if len(record.keys()) < 3:
-            #     df.loc[df['sentiment'] == record['sentiment'],'count'] = record['count']
-            #     print(df)
-            #     continue
-            if len(record.keys()) > 3:
-                time_list.append(time.time())
-            yield (f"data:{json.dumps(record)}\n\n")
-    except KeyboardInterrupt:
-        df = pd.DataFrame({"time":time_list})
-        df.to_csv('HSD_time1.csv')
-        consumer.close()
-        print('Saved!')
-    finally:
-        df = pd.DataFrame({"time":time_list})
-        df.to_csv('HSD_time2.csv')
-        consumer.close()
-        print('consumer closed')
             
 if __name__ == "__main__":
     app.run(debug=True)
